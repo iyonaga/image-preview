9101a0e0db69cdb343625ce8bc7421a3ac62e337
iyonaga <yiyonaga@gmail.com>
2018-12-29 23:38:54 +0900

    update querySelector

--- spec/image-preview-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ImagePreviewView from '../lib/image-preview-view';
-
-describe('ImagePreviewView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
